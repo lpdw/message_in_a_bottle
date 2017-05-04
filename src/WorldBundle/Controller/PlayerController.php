8845c6bb6be36e2893df9652d4b64cd67c9ef7c3
AlexandreLac <alexandrelac248@gmail.com>
2017-05-04 16:36:13 +0200

    Update action player

--- src/WorldBundle/Controller/PlayerController.php
+++ src/WorldBundle/Controller/PlayerController.php
@@ -7,8 +7,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use WorldBundle\Entity\Player;
 use WorldBundle\Entity\Item;
+use WorldBundle\Entity\Bottle;
 use WorldBundle\Form\ItemType;
+use WorldBundle\Entity\WorldGame;
 
+/**
+ * Player controller.
+ *
+ * @Route("player")
+ */
 class PlayerController extends Controller
 {
 	/**
@@ -16,16 +23,49 @@ class PlayerController extends Controller
      */
     public function testAction()
     {
-    	$player = new Player();
-    	$item1 = $this->getDoctrine()->getRepository('WorldBundle:Item')->findOneById(1);
-    	$item2 = $this->getDoctrine()->getRepository('WorldBundle:Item')->findOneById(2);
-    	$item3 = $this->getDoctrine()->getRepository('WorldBundle:Item')->findOneById(3);
-    	$objects = array();
-    	$objects[] = array('item' => $item1, 'quantity' => 25);
-    	$objects[] = array('item' => $item2, 'quantity' => 35);
-    	$objects[] = array('item' => $item3, 'quantity' => 15);
-    	$this->addObjectsAction($objects, $player);
-    	// dump($player->randomisTrue(50));
+        $WorldGame = $this->getDoctrine()->getRepository('WorldBundle:WorldGame')->findOneById(3);
+        $Island = $this->getDoctrine()->getRepository('WorldBundle:Island')->findOneById(5);
+        $Player = $this->getDoctrine()->getRepository('WorldBundle:Player')->findOneById(2);
+        // dump($WorldGame);
+        // $player = new Player();
+        // $player->setName("Kangoo");
+        // $player->setStatus("Alive");
+        $em = $this->getDoctrine()->getManager();
+        // $em->persist($player);
+        // $em->flush($player);
+
+        // dump($Island);
+        // $Island->addPlayer($Player);
+        // $em->persist($Player);
+        // $em->flush($Player);
+        // Test Add object in inventory
+        // $item1 = $this->getDoctrine()->getRepository('WorldBundle:Item')->findOneById(1);
+        // $item2 = $this->getDoctrine()->getRepository('WorldBundle:Item')->findOneById(2);
+        // $item3 = $this->getDoctrine()->getRepository('WorldBundle:Item')->findOneById(3);
+        // $objects = array();
+        // $objects[] = array('item' => $item1, 'quantity' => 25);
+        // $objects[] = array('item' => $item2, 'quantity' => 35);
+        // $objects[] = array('item' => $item3, 'quantity' => 15);
+        // $this->addObjectsAction($objects, $player);
+        // dump($player->randomisTrue(50));
+        dump($Player);
+
+
+
+
+
+        // Test look sea
+        // dump($Player->watchSea(15));
+
+        // Test send bottle
+        // dump($Player->launchBottle("NORD","Je suis en bas "));
+
+        // Test swimming
+        dump($Player->swimming("SUD EST"));
+
+        // Test navigate
+        // dump($Player->navigate("SUD"));
+        dump($Player);
     	die();
         return $this->render('WorldBundle:Default:index.html.twig');
     }
@@ -35,10 +75,16 @@ class PlayerController extends Controller
      */
     public function addObjectsAction($objects, $player)
     {
-    	dump($player->PickObject($objects));
+    	$retour = $player->PickObject($objects);
+        if($retour['valid']){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($player);
+            $em->flush($player);
+            dump($retour['value']);
+        }
+        else{dump($retour['value']);}
     }
-
-
+    
     /**
      * @Route("/item/new", name="new_item")
      */
--- src/WorldBundle/Entity/Player.php
+++ src/WorldBundle/Entity/Player.php
@@ -210,35 +210,53 @@ class Player
     public function launchBottle($cardinal,$message)
     {
         $bottle = new Bottle();
-        $bottle->message = $message;
+        $bottle->setMessage($message);
+        
+        $island = $this->findIsland($cardinal)['data'];
 
-        //TODO définir ce qu'il y a dans la direction choisi et la durée de visibilité a définir 
+        return (!$island) ? false: true;
     }
 
     /**
-    * Swimming in the return true if success
+    * Swimming for another island
     *
     * @return boolean
     */
     public function swimming($cardinal)
     {
-        //TODO détecter si il y a une île dans cette direction et calcul sa proba selon la distance
+        $island = $this->findIsland($cardinal);
+        if(!$island['data']){return false;}
+        else{
+            $percent = 50 - (int)($island['distance']*10);
+            if($percent <= 0){return false;}
+            $resultat = $this->randomisTrue($percent);
+            if($resultat){$this->island = $island['data'];}            
+            return $resultat;
+        }
     }
 
     /**
-    * Navigate in the return true if success
+    * Navigate for another island
     *
     * @return boolean
     */
     public function navigate($cardinal)
     {
-        //TODO détecter si il y a une île dans cette direction et calcul sa proba selon la distance
+        $island = $this->findIsland($cardinal);
+        if(!$island['data']){return false;}
+        else{
+            $percent = 100 - (int)(($island['distance']/3)*10);
+            if($percent <= 0){return false;}
+            $resultat = $this->randomisTrue($percent);
+            if($resultat){$this->island = $island['data'];}            
+            return $resultat;
+        }
     }
 
     /**
     *   Watch sea and return message
     *
-    * @return string
+    * @return string/array
     */
     public function watchSea($position)
     {
@@ -251,17 +269,19 @@ class Player
             5 => "Une tempête approche",
             6 => "Les vagues sont hautes",
         );
-        $this->detectIles(2);
+        $islands = $this->detectIslands(2);
         // TODO faire la condition si on voit le bateau du capitaine
-        if(true == false){
-            dump("Le bateau du capitaine est la ");
-        }
-        // TODO faire la condition si on voit un bateau ou une île proche
-        else if(true !== true){
-            dump("Il y a un bateau dans cette direction ");
+        if(!empty($islands)){
+            $msg = array();
+            foreach ($islands as $key => $value) {
+                $base = "une île";
+                if($value['type'] == 'captain'){$base = "l'île du capitaine";}
+                $msg[] = "Il y a $base dans la direction {$value['position']} qui se trouve à ".($value['distance']*10)." minutes";
+            }
+            return $msg;
         }
         else{
-            dump($messages[rand(0,6)]);
+            return $messages[rand(0,6)];
         }
     }
 
@@ -270,14 +290,92 @@ class Player
     * Detect îles 
     * @return array
     */
-    public function detectIles($distance){
-        for ($i=$this->island->getLocalisationX()-$distance; $i <= $this->island->getLocalisationX()+$distance; $i++) { 
-            for ($a=$this->island->getLocalisationY()-$distance; $a <= $this->island->getLocalisationY()+$distance ; $a++) {
+    public function detectIslands($distance){
+        $islands = array();
+        if($distance == "all"){
+            $xStart = $yStart = 0;
+            $xEnd = $yEnd = 14;
+        }
+        else{
+            $xStart = ($this->island->getLocalisationX()-$distance < 0) ? 0 :$this->island->getLocalisationX()-$distance;
+            $xEnd = ($this->island->getLocalisationX()+$distance > 14) ? 14 : $this->island->getLocalisationX()+$distance;
+            $yStart = ($this->island->getLocalisationY()-$distance < 0) ? 0 : $this->island->getLocalisationY()-$distance;
+            $yEnd = ($this->island->getLocalisationY()+$distance > 14) ? 14 : $this->island->getLocalisationY()+$distance;            
+        } 
+        for ($i=$xStart; $i <= $xEnd; $i++) { 
+            for ($a=$yStart; $a <= $yEnd; $a++) {
                 if($i == $this->island->getLocalisationX() && $a == $this->island->getLocalisationY()){continue;}
-                if(gettype($this->worldgame->getGrid()[$i][$a]) == 'object'){
-                    dump($this->worldgame->getGrid()[$i][$a]);
+                if(gettype($this->island->getWorldgame()->getGrid()[$i][$a]) == 'object'){
+                    $pos = $this->directionIsland($this->island->getLocalisationX(),$i,"x");
+                    $pos .= (empty($pos))? $this->directionIsland($this->island->getLocalisationY(),$a,"y") : " ".$this->directionIsland($this->island->getLocalisationY(),$a,"y");
+                    if(substr($pos, -1) == " "){$pos = trim($pos);}
+                    $dist = $this->distanceIslands($this->island->getLocalisationX(),$this->island->getLocalisationY(),$i,$a);
+                    $islands[] = array("cardinal"=>$pos,"distance"=>$dist,"type"=>$this->island->getWorldgame()->getGrid()[$i][$a]->getType(),"x"=>$i,"y"=>$a);
                 }
             }
         }
+        return $islands;
+    }
+
+    /**
+    * Give the direction of ile
+    * @return string
+    */
+    public function directionIsland($x,$i,$type){
+        if($x < $i){return ($type == 'x') ? "SUD" : "EST";}
+        else if($x > $i){return ($type == 'x') ? "NORD" : "OUEST";}
+        return "";
+    }
+
+    /**
+    * Give the distance between two îles
+    * @return string
+    */
+    public function distanceIslands($x,$y,$a,$b){
+        $x = abs($x - $a);
+        $y = abs($y - $b);
+        if($x == 0 || $y == 0){return $x+$y;}
+        else{return ($x+$y)/2;}
+    }
+
+    /**
+    * Move in world
+    * @return array
+    */
+    public function moveinWorld($position,$cardinal){
+        if(strpos(strtoupper($cardinal),"NORD")){$position['x']--;}
+        else if (strpos(strtoupper($cardinal),"SUD")){$position['x']++;}
+
+        if(strpos(strtoupper($cardinal),"EST")){$position['y']++;}
+        else if (strpos(strtoupper($cardinal),"OUEST")){$position['y']--;}
+        return $position;
+    }
+
+    /**
+    * Find ile
+    * @return island
+    */
+    public function findIsland($cardinal){
+        $island = $this->isIsland($cardinal);
+        if($island["x"] == -1 || $island["y"] == -1){return false;}
+        else{return array("data"=>$this->island->getWorldgame()->getGrid()[$island["x"]][$island["y"]],"distance"=>$island['distance']);}
+    }
+
+
+    /**
+    * test if is in island
+    * @return $position
+    */
+    public function isIsland($cardinal){
+        $position = array("x"=>-1, "y"=>-1, "distance" => 14);
+        $islands = $this->detectIslands("all");
+        foreach ($islands as $key => $value) {
+            if($value["cardinal"] == $cardinal && $value["distance"] < $position['distance']){
+                $position['x'] = $value["x"];
+                $position['y'] = $value["y"];
+                $position['distance'] = $value["distance"];
+            }
+        }
+        return $position;
     }
 }
