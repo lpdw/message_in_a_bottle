4ffea169d50e042d6959b2d4aa7a08d24659936e
Joshua Vegas <vegas.joshua@gmail.com>
2017-05-04 16:30:52 +0200

    Add first version of connexion display.

--- src/WorldBundle/Form/RegistrationType.php
+++ src/WorldBundle/Form/RegistrationType.php
@@ -13,13 +13,13 @@ class RegistrationType extends AbstractType
         $builder
             ->add('firstname')
             ->add('lastname')
-            ->add('username', null, array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'))
-            ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
+            ->add('username', null, array('label' => 'Username', 'translation_domain' => 'FOSUserBundle'))
+            ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'Email', 'translation_domain' => 'FOSUserBundle'))
             ->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array(
                 'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
                 'options' => array('translation_domain' => 'FOSUserBundle'),
-                'first_options' => array('label' => 'form.password'),
-                'second_options' => array('label' => 'form.password_confirmation'),
+                'first_options' => array('label' => 'Password'),
+                'second_options' => array('label' => 'Password confirmation'),
                 'invalid_message' => 'fos_user.password.mismatch',
             ))
         ;
