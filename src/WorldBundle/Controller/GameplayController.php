e75a7c8b6e39af9829967a9419ded0710cda8434
Vaultz <paulvialart@hotmail.fr>
2017-05-31 16:25:24 +0200

    Adding a testy "takeBottle" function in the Hut

--- src/WorldBundle/Controller/GameplayController.php
+++ src/WorldBundle/Controller/GameplayController.php
@@ -3,9 +3,11 @@
 namespace WorldBundle\Controller;
 
 use WorldBundle\Entity\WorldGame;
+use WorldBundle\Entity\Hut;
 use WorldBundle\Entity\Player;
 use WorldBundle\Entity\Inventory;
 use WorldBundle\Entity\Item;
+use WorldBundle\Entity\Bottle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -117,6 +119,33 @@ class GameplayController extends Controller
          return new Response(201);
       }
 
+      /**
+      * @Route("/takebottle/{id}", name="take_bottle")
+      * Testy function that just creates a bottle out of litteraly nowhere and add it to the current player's inventory
+      * @param id_hut
+      */
+      public function takeBottle($id) {
+          $em = $this->getDoctrine()->getManager();
+
+          $bottle = new Bottle();
+          $bottle->setName('bottle');
+          $bottle->setDescription('A glassy bottle with a paper in it. Your best friend against solitude.');
+          $bottle->setImage('no_image');
+          $bottle->setMessage("");
+          $em->persist($bottle);
+
+          $item = array("quantity"=>1, "item"=>$bottle);
+
+          $user = $this->get('security.token_storage')->getToken()->getUser();
+          $player = $user->getPlayers()->last();
+          $player->getInventory()->addItem($item);
+          $em->persist($player);
+
+          $em->flush();
+
+          return $this->redirectToRoute('hut_show', array('id' => $id));
+
+      }
 
 
 }
