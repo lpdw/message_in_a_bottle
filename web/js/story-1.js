7e3114317f1b8fe514f481a121623305d15f5ee5
kerfendal yann <kerfendalyann@gmail.com>
2017-05-31 17:01:59 +0200

    ajout de reponse random pour script foret

--- web/js/story-1.js
+++ web/js/story-1.js
@@ -20,24 +20,34 @@ $( document ).ready(function() {
             var i = $(this).attr('id');
             console.log("l'id est égal à : "+i);
 
-
-
-
-
-
-
+            // recupere les reponses de la question correspondant a l'id
             var q = data.story[i].reponse;
             console.log('les réponses sont : '+q)
 
+            // on vide la story
             $(".story").empty();
             console.log(q.length);
 
-
+            // on affiche la description de la question
             $(".story").append("<p>"+data.story[i].desc+"</p></br>");
 
-            for(a = 0; a < q.length; a++){
-                $('.story').append("<button class='buttonForest' id="+q[a]+">"+data.story[q[a]].action+"</button>");
+            if (data.story[i].style == "choice")
+            {
+              for(a = 0; a < q.length; a++)
+              {
+                  $('.story').append("<button class='buttonForest' id="+q[a]+">"+data.story[q[a]].action+"</button>");
+              };
             }
+            // on affiche les questions de maniere aléatoire suivantes si style = random
+            else
+            {
+              console.log("nombre de reponses : "+q.length);
+              a = Math.floor(Math.random()*q.length);
+              console.log("reponse donnee: "+a);
+              $('.story').append("<button class='buttonForest' id="+q[a]+">"+data.story[q[a]].action+"</button>");
+
+            };
+
 
 
 
